04c908e6c11a1c29e7419e651c460acffeeb7fe8
Matthew Miller <Matthew.Miller@acadian.com>
2018-06-22 19:11:48 -0500

    WIP  Adds example WPF app Contact Manager.

---
+++ examples/ContactsManager/ContactsManager/ContactEdit.fs
@@ -0,0 +1,88 @@
+﻿module ContactEdit
+
+open System
+open System.Windows
+open VinylUI
+open VinylUI.Wpf
+open Domain
+
+type Model = {
+    FirstName: Result<string, string>
+    LastName: Result<string, string>
+    Groups: string list
+    Group: string option
+    Numbers: ContactNumber ResizeArray
+    Notes: string
+    Result: Contact option
+}
+
+type Events =
+    | Save
+    | Cancel
+
+module Errors =
+    let nameBlank typ = Error <| sprintf "%s name cannot be blank" typ
+
+type View = FsXaml.XAML<"ContactEditWindow.xaml">
+
+let binder (view: View) model =
+    let nonEmpty typ s =
+        if String.IsNullOrWhiteSpace s then Errors.nameBlank typ
+        else Ok s
+
+    view.NumberGrid.ItemsSource <- model.Numbers
+
+    [ Bind.view(<@ view.FirstNameBox.Text @>).toModelResult(<@ model.FirstName @>, nonEmpty "First")
+      Bind.view(<@ view.LastNameBox.Text @>).toModelResult(<@ model.LastName @>, nonEmpty "Last")
+      Bind.model(<@ model.Groups @>).toItemsSource(view.GroupCombo)
+      Bind.view(<@ view.GroupCombo.Text @>).toModel(<@ model.Group @>)
+      Bind.view(<@ view.NotesBox.Text @>).toModel(<@ model.Notes @>)
+    ]
+
+let events (view: View) =
+    [ view.SaveButton.Click |> Observable.mapTo Save
+      view.CancelButton.Click |> Observable.mapTo Cancel
+    ]
+
+let save close model =
+    match model.FirstName, model.LastName with
+    | Ok first, Ok last ->
+        let contact = 
+            { FirstName = first
+              LastName = last
+              Group = model.Group
+              Numbers = model.Numbers |> Seq.toList
+              Notes = model.Notes
+            }
+        close ()
+        { model with Result = Some contact }
+    | Error e, _ | _, Error e ->
+        MessageBox.Show(e, "Invalid Information", MessageBoxButton.OK, MessageBoxImage.Warning) |> ignore
+        model
+
+let dispatcher (close: unit -> unit) = function
+    | Save -> Sync (save close)
+    | Cancel -> Sync (fun m -> close(); m)
+
+let start groups (contact: Contact option) (view: View) =
+    let model =
+        match contact with
+        | Some c ->
+            { FirstName = Ok c.FirstName
+              LastName = Ok c.LastName
+              Groups = groups
+              Group = c.Group
+              Numbers = c.Numbers |> ResizeArray
+              Notes = c.Notes
+              Result = None
+            }
+        | None ->
+            { FirstName = Errors.nameBlank "First"
+              LastName = Errors.nameBlank "Last"
+              Groups = groups
+              Group = None
+              Numbers = ResizeArray()
+              Notes = ""
+              Result = None
+            }
+    Framework.start binder events (dispatcher view.Close) view model
---
+++ examples/ContactsManager/ContactsManager/Domain.fs
@@ -0,0 +1,70 @@
+﻿namespace Domain
+
+type NumberType =
+    | Mobile
+    | Work
+    | Home
+    | Fax
+    | Other of string
+with
+    override this.ToString () =
+        match this with
+        | Other t -> t
+        | _ -> sprintf "%A" this
+
+    static member Choices = [
+        Mobile
+        Work
+        Home
+        Fax
+    ]
+
+module Format =
+    let phone (number: string) =
+        let insert c i (s: string) =
+            s.Substring(0, i) + c + s.Substring(i)
+        if number.Length = 10 then
+            number |> insert "-" 6 |> insert ") " 3 |> insert "(" 0
+        else if number.Length = 7 then
+            number |> insert "-" 3
+        else number
+
+[<CLIMutable>]
+type ContactNumber = {
+    Number: string
+    Type: NumberType
+} with
+    override this.ToString () =
+        sprintf "%s: %s" (string this.Type) (Format.phone this.Number)
+
+type Contact = {
+    FirstName: string
+    LastName: string
+    Group: string option
+    Numbers: ContactNumber list
+    Notes: string
+} with
+    member this.FullName =
+        sprintf "%s %s" this.FirstName this.LastName
+
+    member this.PrimaryNumber =
+        this.Numbers |> List.tryHead |> Option.map (string)
+
+
+open System.Windows.Data
+
+type NumberTypeConverter() =
+    interface IValueConverter with
+        member this.Convert (value, _, _, _) =
+            match value :?> NumberType with
+            | Other s -> s |> box
+            | t -> sprintf "%A" t |> box
+
+        member this.ConvertBack (value, _, _, _) =
+            let typ = value :?> string
+            let case =
+                Reflection.FSharpType.GetUnionCases(typeof<NumberType>)
+                |> Seq.tryFind (fun c -> c.Name.ToLower() = typ.ToLower())
+            match case with
+            | Some c -> Reflection.FSharpValue.MakeUnion(c, [||])
+            | None -> Other typ |> box
